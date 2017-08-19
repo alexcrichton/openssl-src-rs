974d3ec74e4947aa7044099709d0f9acd19f700c
Alex Crichton <alex@alexcrichton.com>
2017-08-19 16:40:29 -0700

    Start adding Windows

--- src/lib.rs
+++ src/lib.rs
@@ -65,17 +65,45 @@ impl Build {
 
         let mut configure = Command::new("perl");
         configure.arg("./Configure");
+        if target.contains("pc-windows-gnu") {
+            configure.arg(&format!("--prefix={}", sanitize_sh(&install_dir)));
+        } else {
+            configure.arg(&format!("--prefix={}", install_dir.display()));
+        }
+
         configure
+            // No shared objects, we just want static libraries
             .arg("no-dso")
+            .arg("no-shared")
+
+            // Should be off by default on OpenSSL 1.1.0, but let's be extra sure
             .arg("no-ssl3")
-            .arg("no-comp")
+
+            // No need to build tests, we won't run them anyway
             .arg("no-unit-test")
+
+            // Nothing related to zlib please
+            .arg("no-comp")
             .arg("no-zlib")
             .arg("no-zlib-dynamic")
-            .arg("no-shared");
 
-        if target.contains("unknown-linux-musl") {
-            configure.arg("no-engine");
+            // This actually fails to compile on musl (it needs linux/version.h
+            // right now) but we don't actually need this most of the time. This
+            // is intended for super-configurable backends and whatnot
+            // apparently but the whole point of this script is to produce a
+            // "portable" implementation of OpenSSL, so shouldn't be any harm in
+            // turning this off.
+            .arg("no-engine");
+
+        if target.contains("msvc") {
+            // On MSVC we need nasm.exe to compile the assembly files, but let's
+            // just pessimistically assume for now that's not available.
+            configure.arg("no-asm");
+        } else {
+            // If we're *not* on MSVC then we can optimize our build a bit by
+            // avoiding building the CLI tools. Unfortunately though on MSVC if
+            // we pass this option the build breaks oddly...
+            configure.arg("no-stdio");
         }
 
         let os = match target {
@@ -106,54 +134,87 @@ impl Build {
             "aarch64-linux-android" => "android64-aarch64",
             "i686-linux-android" => "android-x86",
             "x86_64-linux-android" => "android64",
+            "x86_64-pc-windows-msvc" => "VC-WIN64A",
+            "i686-pc-windows-msvc" => "VC-WIN64A",
             _ => panic!("don't know how to configure OpenSSL for {}", target),
         };
 
         configure.arg(os);
 
-        let mut gcc = gcc::Build::new();
-        gcc.target(target)
-            .host(host)
-            .warnings(false)
-            .opt_level(2);
-        let compiler = gcc.get_compiler();
-        configure.env("CC", compiler.path());
-        let path = compiler.path().to_str().unwrap();
-        if path.ends_with("-gcc") && !target.contains("unknown-linux-musl") {
-            let path = &path[..path.len() - 4];
-            configure.env("RANLIB", format!("{}-ranlib", path));
-            configure.env("AR", format!("{}-ar", path));
-        }
-        for arg in compiler.args() {
-            configure.arg(arg);
-        }
+        // If we're not on MSVC we configure cross compilers and cross tools and
+        // whatnot. Note that this doesn't happen on MSVC b/c things are pretty
+        // different there and this isn't needed most of the time anyway.
+        if !target.contains("msvc") {
+            let mut gcc = gcc::Build::new();
+            gcc.target(target)
+                .host(host)
+                .warnings(false)
+                .opt_level(2);
+            let compiler = gcc.get_compiler();
+            configure.env("CC", compiler.path());
+            let path = compiler.path().to_str().unwrap();
 
-        if target.contains("android") && self.cross_sysroot.is_none() {
-            for path in env::split_paths(&env::var_os("PATH").unwrap()) {
-                if !path.join(compiler.path()).exists() {
-                    continue
+            // Infer ar/ranlib tools from cross compilers if the it looks like
+            // we're doing something like `foo-gcc` route that to `foo-ranlib`
+            // as well.
+            if path.ends_with("-gcc") && !target.contains("unknown-linux-musl") {
+                let path = &path[..path.len() - 4];
+                configure.env("RANLIB", format!("{}-ranlib", path));
+                configure.env("AR", format!("{}-ar", path));
+            }
+
+            // Make sure we pass extra flags like `-ffunction-sections` and
+            // other things like ARM codegen flags.
+            for arg in compiler.args() {
+                configure.arg(arg);
+            }
+
+            // Not really sure why, but on Android specifically the
+            // CROSS_SYSROOT variable needs to be set. The build system will
+            // pass `--sysroot=$(CROSS_SYSROOT)` so we need to make sure that's
+            // set to something. By default we infer it as next to the `bin`
+            // directory containing the compiler itself.
+            if target.contains("android") && self.cross_sysroot.is_none() {
+                for path in env::split_paths(&env::var_os("PATH").unwrap()) {
+                    if !path.join(compiler.path()).exists() {
+                        continue
+                    }
+                    let path = path.parent().unwrap(); // chop off 'bin'
+                    self.cross_sysroot = Some(path.join("sysroot"));
+                    break
                 }
-                let path = path.parent().unwrap(); // chop off 'bin'
-                self.cross_sysroot = Some(path.join("sysroot"));
-                break
             }
         }
 
+        // And finally, run the perl configure script!
         configure.current_dir(&inner_dir);
         self.run_command(configure, "configuring OpenSSL build");
 
-        let mut depend = Command::new("make");
-        depend.arg("depend").current_dir(&inner_dir);
-        self.run_command(depend, "building OpenSSL dependencies");
+        // On MSVC we use `nmake.exe` with a slightly different invocation, so
+        // have that take a different path than the standard `make` below.
+        if target.contains("msvc") {
+            let mut build = gcc::windows_registry::find(target, "nmake.exe")
+                .expect("failed to find nmake");
+            build.current_dir(&inner_dir);
+            self.run_command(build, "building OpenSSL");
 
-        let mut build = Command::new("make");
-        build.current_dir(&inner_dir);
-        self.run_command(build, "building OpenSSL");
+            let mut install = gcc::windows_registry::find(target, "nmake.exe")
+                .expect("failed to find nmake");
+            install.arg("install").current_dir(&inner_dir);
+            self.run_command(install, "installing OpenSSL");
+        } else {
+            let mut depend = Command::new("make");
+            depend.arg("depend").current_dir(&inner_dir);
+            self.run_command(depend, "building OpenSSL dependencies");
 
-        let mut install = Command::new("make");
-        install.arg("install").current_dir(&inner_dir)
-            .arg(format!("DESTDIR={}", install_dir.display()));
-        self.run_command(install, "installing OpenSSL");
+            let mut build = Command::new("make");
+            build.current_dir(&inner_dir);
+            self.run_command(build, "building OpenSSL");
+
+            let mut install = Command::new("make");
+            install.arg("install").current_dir(&inner_dir);
+            self.run_command(install, "installing OpenSSL");
+        }
 
         (lib_dir, include_dir)
     }
@@ -165,9 +226,13 @@ impl Build {
         let status = command.status().unwrap();
         if !status.success() {
             panic!("
-    Error {}:
-        Command: {:?}
-        Exit status: {}
+
+
+Error {}:
+    Command: {:?}
+    Exit status: {}
+
+
     ",
                 desc,
                 command,
@@ -189,6 +254,26 @@ fn cp_r(src: &Path, dst: &Path) {
             let _ = fs::remove_file(&dst);
             fs::copy(&path, &dst).unwrap();
         }
+    }
+}
+
+fn sanitize_sh(path: &Path) -> String {
+    if !cfg!(windows) {
+        return path.to_str().unwrap().to_string()
+    }
+    let path = path.to_str().unwrap().replace("\\", "/");
+    return change_drive(&path).unwrap_or(path);
+
+    fn change_drive(s: &str) -> Option<String> {
+        let mut ch = s.chars();
+        let drive = ch.next().unwrap_or('C');
+        if ch.next() != Some(':') {
+            return None
+        }
+        if ch.next() != Some('/') {
+            return None
+        }
+        Some(format!("/{}/{}", drive, &s[drive.len_utf8() + 2..]))
     }
 }
 
