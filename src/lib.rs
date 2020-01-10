f8aa5b2e34387c0b0aeae403c6e1b4898b5b2958
Franck Royer <royer.franck@gmail.com>
2020-01-11 03:01:53 +1100

    Allow to specify custom path for Perl (#49)
    * Allow to specify custom path for Perl
    
    Env var OPENSSL_SRC_PERL can be set to specify
    a path to the perl binary to use to call the openssl
    perl scripts.
    
    Resolves #45
    
    * Fallback to PERL env var if OPENSSL_SRC_PERL is not set

--- src/lib.rs
+++ src/lib.rs
@@ -77,7 +77,9 @@ impl Build {
         cp_r(&source_dir(), &inner_dir);
         apply_patches(target, &inner_dir);
 
-        let mut configure = Command::new("perl");
+        let perl_program =
+            env::var("OPENSSL_SRC_PERL").unwrap_or(env::var("PERL").unwrap_or("perl".to_string()));
+        let mut configure = Command::new(perl_program);
         configure.arg("./Configure");
         if host.contains("pc-windows-gnu") {
             configure.arg(&format!("--prefix={}", sanitize_sh(&install_dir)));
