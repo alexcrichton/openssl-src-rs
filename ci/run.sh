6d9bf2c7317b0b955ecb2a89ea5a57288f0f27dd
Jake Goulding <jake.goulding@gmail.com>
2020-10-16 10:59:52 -0400

    Add aarch64-apple-darwin to CI (#77)

--- ci/run.sh
+++ ci/run.sh
@@ -1,5 +1,13 @@
 target=$1
 set -ex
+
+if [ "$1" = "aarch64-apple-darwin" ] ; then
+	sudo xcode-select -s /Applications/Xcode_12.2.app/Contents/Developer/
+	export SDKROOT=$(xcrun -sdk macosx11.0 --show-sdk-path)
+	export MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk macosx11.0 --show-sdk-platform-version)
+	export CARGO_TARGET_AARCH64_APPLE_DARWIN_RUNNER=echo
+fi
+
 cargo test --manifest-path testcrate/Cargo.toml --target $1 -vv
 cargo test --manifest-path testcrate/Cargo.toml --target $1 -vv --release
 if [ "$1" = "x86_64-unknown-linux-gnu" ] ; then
