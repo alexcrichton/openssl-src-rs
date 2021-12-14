466ffd201be07d0137e373de3aac2349b9063e93
Eric Huss <eric@huss.org>
2021-12-14 15:33:10 -0800

    Fix aarch64-apple-darwin CI (#116)

--- ci/run.sh
+++ ci/run.sh
@@ -5,9 +5,6 @@ testcrate_dir="$(pwd)/testcrate"
 set -ex
 
 if [ "$1" = "aarch64-apple-darwin" ] ; then
-  sudo xcode-select -s /Applications/Xcode_12.2.app/Contents/Developer/
-  export SDKROOT=$(xcrun -sdk macosx11.0 --show-sdk-path)
-  export MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk macosx11.0 --show-sdk-platform-version)
   export CARGO_TARGET_AARCH64_APPLE_DARWIN_RUNNER=echo
 fi
 
