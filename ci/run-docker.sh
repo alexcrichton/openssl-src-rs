5e7150684599062a8781292a3ed6efa32dc9e29c
Georg Semmler <georg.semmler@giga-infosystems.com>
2025-10-15 14:30:27 +0200

    Add some testing to ci
    This commit changes te CI setup to run on all platforms with the
    packaged source code instead of the source code in the repository. This
    is usful for ensuring that everything required is included. This is the
    preparation for excluding more files from the release.
    
    Additionally this bumps the checkout action to version 5.

--- ci/run-docker.sh
+++ ci/run-docker.sh
@@ -3,24 +3,23 @@
 target=$1
 
 if [ ! -d ci/docker/$1 ]; then
-  exec ci/run.sh $1
+    exec ci/run.sh $1
 fi
 
 set -ex
 
 docker build \
-  --rm \
-  --tag openssl-src-ci \
-  ci/docker/$1
+    --rm \
+    --tag openssl-src-ci \
+    ci/docker/$1
 
 docker run \
-  --rm \
-  --volume `rustc --print sysroot`:/rust:ro \
-  --volume `pwd`:/usr/code:ro \
-  --volume `pwd`/target:/usr/code/target \
-  --volume $HOME/.cargo:/cargo \
-  --env CARGO_HOME=/cargo \
-  --workdir /usr/code \
-  openssl-src-ci \
-  bash -c "PATH=\"\$PATH:/rust/bin:/cargo/bin\" ci/run.sh $target"
-
+    --rm \
+    --volume $(rustc --print sysroot):/rust:ro \
+    --volume $(pwd):/usr/code:rw \
+    --volume $(pwd)/target:/usr/code/target \
+    --volume $HOME/.cargo:/cargo \
+    --env CARGO_HOME=/cargo \
+    --workdir /usr/code \
+    openssl-src-ci \
+    bash -c "PATH=\"\$PATH:/rust/bin:/cargo/bin\" ci/run.sh $target"
