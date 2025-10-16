9e65e53b7e95e93c851d6b02517ec2d292c1f244
Georg Semmler <georg.semmler@giga-infosystems.com>
2025-10-16 21:36:44 +0000

    Remove more files from the published package (#273)
    * Add some testing to ci
    
    This commit changes te CI setup to run on all platforms with the
    packaged source code instead of the source code in the repository. This
    is usful for ensuring that everything required is included. This is the
    preparation for excluding more files from the release.
    
    Additionally this bumps the checkout action to version 5.
    
    * Exclude more stuff
    
    This commit excludes more files from the openssl source code from the
    code uploaded to crates.io. All of the excluded files are not required
    to build openssl on any target listed on ci.
    
    Before: 3699 files, 44.5MiB (10.0MiB compressed)
    After: 2383 files, 31.6MiB (6.8MiB compressed)

--- ci/run.sh
+++ ci/run.sh
@@ -10,29 +10,6 @@ export CARGO_TARGET_AARCH64_APPLE_DARWIN_RUNNER=echo
 cargo test --manifest-path "$testcrate_dir/Cargo.toml" --target $target -vvv
 cargo test --manifest-path "$testcrate_dir/Cargo.toml" --target $target -vvv --release
 
-if [ "$1" = "x86_64-unknown-linux-gnu" ] ; then
-  cargo test --manifest-path "$testcrate_dir/Cargo.toml" --target $target -vvv --all-features
-
-  # Run a few tests here:
-  #
-  # * Make sure the packaged crate file isn't bigger than 10MB which is
-  #   crate.io's limit.
-  # * Make sure that the package crate itself works.
-  #
-  # A lot of OpenSSL's source code is excluded on crates.io because it makes the
-  # crate file much too large, so the test here should inform us if we're
-  # missing anything actually required to build OpenSSL.
-  rm -rf target/ci
-  cargo package --allow-dirty --target-dir target/ci
-  crate=`ls target/ci/package/*.crate`
-  filesize=$(stat -c%s "$crate")
-  echo "tarball is $filesize bytes"
-  if (( filesize > 15000000 )); then
-    echo "file size too big"
-    exit 1
-  fi
-  rm "$crate"
-  cd target/ci/package/openssl-src-*
-  cp -r "$testcrate_dir" .
-  cargo test --manifest-path "testcrate/Cargo.toml" --target $target -vv
+if [ "$1" = "x86_64-unknown-linux-gnu" ]; then
+    cargo test --manifest-path "$testcrate_dir/Cargo.toml" --target $target -vvv --all-features
 fi
